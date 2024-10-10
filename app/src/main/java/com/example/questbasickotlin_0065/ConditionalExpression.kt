d3cb81ce4381c03b49a63f4e46206bbd273e7ded
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-10 19:04:18 +0700

    mendeklarasikan function ConditionalStantement

---
+++ app/src/main/java/com/example/questbasickotlin_0065/ConditionalExpression.kt
@@ -0,0 +1,7 @@
+package com.example.questbasickotlin_0065
+
+//conditional Expresion adalah ekspresi yang mengembalilkan nilai berdasarkan kondisi tertentu
+//conditional expresion menggunakan if-else, when, dan try-catch
+fun ConditionalStantement() {
+    println("=== Conditional Expression ===")
+}
