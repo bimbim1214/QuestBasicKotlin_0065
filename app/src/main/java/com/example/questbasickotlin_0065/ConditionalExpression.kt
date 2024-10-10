f30daab953332c1a4d29be434733992aae2f7e75
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-10 19:10:10 +0700

    membuat variabel number dengan nilai 10 dan untuk mengeceknya menggunakan if-else jika nilai nya lebih besar dari 0 maka number positif jika tidak else maka number negativ

--- app/src/main/java/com/example/questbasickotlin_0065/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0065/ConditionalExpression.kt
@@ -4,4 +4,12 @@ package com.example.questbasickotlin_0065
 //conditional expresion menggunakan if-else, when, dan try-catch
 fun ConditionalStantement() {
     println("=== Conditional Expression ===")
+
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }else {
+        println("Negative Number")
+    }
 }
