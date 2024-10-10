e4a44d85e423cb5690425ec4531eb9718c26e263
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-10 21:46:13 +0700

    membuat function uppercaseString dengan membaut parameter string dengan tipe data string dan mengkonvert bahwa parameter string itu adalah uppercase

---
+++ app/src/main/java/com/example/questbasickotlin_0065/LamdaFunction.kt
@@ -0,0 +1,9 @@
+package com.example.questbasickotlin_0065
+
+//Lamda Function adalah fungsi yang tidak memiliki nama
+//Lamda Function biasanya digunakan untuk membuat fungsi yang sederhana
+//Lamda Function menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
