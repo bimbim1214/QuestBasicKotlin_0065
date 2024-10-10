d49dbeedd60d09b1377dc1c88c7956c15913e0f7
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-10 22:31:50 +0700

    menambahkan parameter nullable dengan tipe data string dengan value "you can keep a null here" dan mendeklarasikan nullable = null

--- app/src/main/java/com/example/questbasickotlin_0065/NullSafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0065/NullSafety.kt
@@ -3,4 +3,12 @@ package com.example.questbasickotlin_0065
 fun nullSafety() {
     //neverNull has String type
     var neverNull: String = "This can't be Null"
+
+    //Throws a compiler error
+    //neverNull = null
+
+    //nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    //This is Ok
+    nullable = null
 }
